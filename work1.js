cfb85d964c74e21a4c5c6ba491f373352fe2c25a
WOOJAE KIM <kimw@ocadu.ca>
2025-11-16 18:03:46 -0500

    gg

--- work1.js
+++ work1.js
@@ -7,6 +7,7 @@ let sliderMinY = 50;
 let sliderMaxY = 0;
 let savedBrightnessLevel = 0;
 let touchCount = 0;
+let lastTouchY = null;
 
 function preload() {
   img1 = loadImage('lighton.jpg');  
@@ -16,20 +17,24 @@ function preload() {
 function setup() {
   let c = createCanvas(windowWidth, windowHeight);
   c.parent('canvasWrap');
+
+  // 모바일에서 캔버스가 터치 독점하지 않도록
+  c.elt.style.touchAction = "none";
+
   currentImg = img2;
-  
+
   sliderMaxY = height - 50 - sliderHeight;
   sliderY = sliderMaxY;
 }
 
 function draw() {
   background(0);
-  
+
   // 이미지 자동 비율 맞춤
   let ar_img = currentImg.width / currentImg.height;
   let ar_win = width / height;
   let drawW, drawH;
-  
+
   if (ar_img > ar_win) {
     drawH = height;
     drawW = height * ar_img;
@@ -37,18 +42,18 @@ function draw() {
     drawW = width;
     drawH = width / ar_img;
   }
-  
+
   imageMode(CENTER);
   image(currentImg, width/2, height/2, drawW, drawH);
-  
-  // 라이팅 효과 (lighton 상태 + brightness > 0일 때만)
+
+  // 라이팅 효과
   if (currentImg === img1 && brightnessLevel > 0) {
     let brightness = map(brightnessLevel, 0.1, 5, 0, 150);
     let radius = map(brightnessLevel, 0.1, 5, 50, 400);
-    
+
     let lightX = width/1.8 + 100;
     let lightY = height/2;
-    
+
     for (let r = radius; r > 0; r -= 10) {
       let alpha = map(r, 0, radius, brightness, 0);
       noStroke();
@@ -56,11 +61,9 @@ function draw() {
       circle(lightX, lightY, r * 2);
     }
   }
-  
-  // 슬라이더 그리기
+
   drawSlider();
-  
-  // 터치 횟수 표시 (중간 상단)
+
   fill(255);
   textAlign(CENTER, TOP);
   textSize(16);
@@ -68,24 +71,19 @@ function draw() {
 }
 
 function drawSlider() {
-  // 슬라이더 트랙
   stroke(100);
   strokeWeight(2);
   line(25, sliderMinY, 25, height - 50);
-  
-  // 슬라이더 바 (원형)
+
   fill(255, 255, 100);
   noStroke();
   circle(25, sliderY + sliderHeight / 2, 16);
 }
 
 function updateBrightness() {
-  // 슬라이더 위치 -> 밝기값 (0~5, 소수점 포함)
   let normalizedPos = 1 - ((sliderY - sliderMinY) / (sliderMaxY - sliderMinY));
-  brightnessLevel = normalizedPos * 5;
-  brightnessLevel = constrain(brightnessLevel, 0, 5);
-  
-  // 밝기 0이면 lightoff, 0보다 크면 lighton
+  brightnessLevel = constrain(normalizedPos * 5, 0, 5);
+
   if (brightnessLevel > 0.1) {
     currentImg = img1;
   } else {
@@ -94,31 +92,12 @@ function updateBrightness() {
 }
 
 function mousePressed() {
-  // 슬라이더 터치 판정
-  let distToSlider = dist(mouseX, mouseY, 25, sliderY + sliderHeight / 2);
-  if (distToSlider < 25) {
+  if (dist(mouseX, mouseY, 25, sliderY + sliderHeight / 2) < 25) {
     isDraggingSlider = true;
-    return false;
+    return;
   }
-  
-  // 슬라이더가 아닌 곳 터치: 이미지 토글
-  touchCount++;
-  
-  if (currentImg === img1) {
-    // lighton -> lightoff
-    currentImg = img2;
-    savedBrightnessLevel = brightnessLevel;
-    brightnessLevel = 0;
-    sliderY = sliderMaxY;
-  } else {
-    // lightoff -> lighton
-    currentImg = img1;
-    brightnessLevel = savedBrightnessLevel > 0 ? savedBrightnessLevel : 2.5;
-    let normalizedBrightness = brightnessLevel / 5;
-    sliderY = sliderMaxY - (normalizedBrightness * (sliderMaxY - sliderMinY));
-  }
-  
-  return false;
+
+  toggleImage();
 }
 
 function mouseDragged() {
@@ -126,7 +105,6 @@ function mouseDragged() {
     sliderY += movedY;
     sliderY = constrain(sliderY, sliderMinY, sliderMaxY);
     updateBrightness();
-    return false;
   }
 }
 
@@ -135,49 +113,54 @@ function mouseReleased() {
 }
 
 function touchStarted() {
-  // 슬라이더 터치 판정
   if (touches.length > 0) {
-    let touchX = touches[0].x;
-    let touchY = touches[0].y;
-    let distToSlider = dist(touchX, touchY, 25, sliderY + sliderHeight / 2);
-    
+    let t = touches[0];
+    let distToSlider = dist(t.x, t.y, 25, sliderY + sliderHeight / 2);
+
     if (distToSlider < 25) {
       isDraggingSlider = true;
-      return false;
+      lastTouchY = t.y;
+      return;
     }
   }
-  
-  // 슬라이더가 아닌 곳 터치: 이미지 토글
-  touchCount++;
-  
-  if (currentImg === img1) {
-    // lighton -> lightoff
-    currentImg = img2;
-    savedBrightnessLevel = brightnessLevel;
-    brightnessLevel = 0;
-    sliderY = sliderMaxY;
-  } else {
-    // lightoff -> lighton
-    currentImg = img1;
-    brightnessLevel = savedBrightnessLevel > 0 ? savedBrightnessLevel : 2.5;
-    let normalizedBrightness = brightnessLevel / 5;
-    sliderY = sliderMaxY - (normalizedBrightness * (sliderMaxY - sliderMinY));
-  }
-  
-  return false;
+
+  toggleImage();
 }
 
 function touchMoved() {
   if (isDraggingSlider && touches.length > 0) {
-    sliderY += (touches[0].y - touches[0].py);
-    sliderY = constrain(sliderY, sliderMinY, sliderMaxY);
-    updateBrightness();
-    return false;
+    let t = touches[0];
+
+    if (lastTouchY !== null) {
+      let dy = t.y - lastTouchY;
+      sliderY += dy;
+      sliderY = constrain(sliderY, sliderMinY, sliderMaxY);
+      updateBrightness();
+    }
+
+    lastTouchY = t.y;
   }
 }
 
 function touchEnded() {
   isDraggingSlider = false;
+  lastTouchY = null;
+}
+
+function toggleImage() {
+  touchCount++;
+
+  if (currentImg === img1) {
+    currentImg = img2;
+    savedBrightnessLevel = brightnessLevel;
+    brightnessLevel = 0;
+    sliderY = sliderMaxY;
+  } else {
+    currentImg = img1;
+    brightnessLevel = savedBrightnessLevel > 0 ? savedBrightnessLevel : 2.5;
+    let normalizedBrightness = brightnessLevel / 5;
+    sliderY = sliderMaxY - (normalizedBrightness * (sliderMaxY - sliderMinY));
+  }
 }
 
 function windowResized() {
